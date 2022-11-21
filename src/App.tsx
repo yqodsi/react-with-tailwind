207f913c4244a8049208d4f8566f6771dcbe6379
MohamedBehhar <bahharmohamed18@gmail.com>
2022-11-21 18:22:02 +0100

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,22 @@
-import { useState, useRef } from "react";
-import TodoList from "../components/TodoList.jsx";
+import React, { useState, useRef, ReactEventHandler } from "react";
+import TodoList from "../components/TodoList";
 import "./index.css";
 import { v4 as uuidv4 } from "uuid";
+import { BrowserRouter, Routes ,Route } from "react-router-dom";
+
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const todoNameRef = useRef();
-  const addTodo = (e) => {
-    const name = todoNameRef.current.value;
-    if (name === "") return null;
-    console.log(name);
-    setTodos((prev) => {
-      return [...prev, { id: uuidv4(), name: name, complete: false }];
-    });
-    todoNameRef.current.value = null;
-  };
+  
   return (
-    <div className="App flex flex-col gap-7 border h-screen items-center">
-      <TodoList todos={todos} />
-      <input
-        ref={todoNameRef}
-        className="border-2 border-gray-400 hover:border-green-400 text-lg focus:border-red-400 rounded-full py-2 px-12"
-        type="text"
-      />
-      <button
-        className="bg-blue-500 hover:bg-blue-600 px-6 w-64 py-2 rounded-md text-white text-xl "
-        onClick={addTodo}
-      >
-        Add Todo
-      </button>
-      <button className="bg-red-500 hover:bg-red-600 px-6 w-64 py-2 rounded-md text-white text-xl ">
-        Clear Completed Todos
-      </button>
-      <div className="text-sm  font-semibold">0 todo left</div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>heyyy</h1>} />
+        <Route path="testing" element={<h1>testing</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+// modal tooltip popover button dropdown navbar form input select switch table card layout 
--- src/main.tsx
+++ src/main.tsx
@@ -2,24 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 import './index.css'
-import Home from '../components/Home'
-import About from '../components/About'
 // import "./App.css";
 import App from "./App"
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-  },
-// {
-//   path: "/about",
-//   element: <About/>
-// }
-]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
